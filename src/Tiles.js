5505757b72f9b447c243254f296220133ef9d451
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-08-26 16:44:26 -0400

    added tiles

---
+++ src/Tiles.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Tiles.css';
+
+class Tiles extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			mark : [0, 1, 2]
+
+		}
+	}
+	
+	clickHandler(){
+		this.setState({
+			
+
+		})
+	}
+}
+
+
+render(){
+
+}
